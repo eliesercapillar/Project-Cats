ddfc88ab9c06a923059db824a03819a97038a9e3
Elieser Capillar <elieser.capillar@hotmail.com>
2024-08-17 22:50:31 -0500

    Modified PlayerController to use the CharacterController component

--- Assets/_Scripts/Player/Movement/PlayerController.cs
+++ Assets/_Scripts/Player/Movement/PlayerController.cs
@@ -5,26 +5,29 @@ using Managers;
 
 namespace Player
 {
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
         [Header("Components")]
-        [SerializeField] private Rigidbody _rb;
+        [SerializeField] private CharacterController _characterController;
 
         [Header("Dependencies")]
         private Camera _camera;
 
         [Header("Movement Properties")]
-        [SerializeField] private float _walkSpeed = 5;
-        [SerializeField] private float _sprintSpeed = 10;
-        [SerializeField] private float _jumpSpeed = 5;
+        [SerializeField] private float _walkSpeed = 2.0f;
+        [SerializeField] private float _sprintSpeed = 5.0f;
+        [SerializeField] private float _jumpHeight = 1.0f;
+        [SerializeField] private float _gravity = -9.81f;
 
         // Internal Properties & State Flags
+        private Vector3 _playerVelocity;
         private Vector2 _movementInputDir;
         private bool _isJumping, _isSprinting;
 
         private void Awake()
         {
-            if (_rb == null) _rb = GetComponent<Rigidbody>();
+            if (_characterController == null) _characterController = GetComponent<CharacterController>();
             if (_camera == null) _camera = Camera.main;
         }
 
@@ -41,27 +44,31 @@ namespace Player
         private void Update()
         {
             Move();
-            Jump();
+            JumpAndApplyGravity();
         }
 
         private void Move()
         {
             if (_movementInputDir == Vector2.zero) return;
-            if (_isSprinting) 
-            {
-                transform.position += new Vector3(_movementInputDir.x, 0, _movementInputDir.y) * _sprintSpeed * Time.deltaTime;
-            }
-            else 
-            {
-                transform.position += new Vector3(_movementInputDir.x, 0, _movementInputDir.y) * _walkSpeed * Time.deltaTime;
-            }
+
+            if (_characterController.isGrounded && _playerVelocity.y < 0) _playerVelocity.y = 0f;
+
+            Vector3 direction = new Vector3(_movementInputDir.x, 0, _movementInputDir.y);
+            direction = _camera.transform.forward * direction.z 
+                      + _camera.transform.right * direction.x;
+            direction.y = 0;
+
+            float speed = _isSprinting ? _sprintSpeed : _walkSpeed;
+
+            _characterController.Move(direction * Time.deltaTime * speed);
         }
 
-        private void Jump()
+        private void JumpAndApplyGravity()
         {
-            if (!_isJumping) return;
+            if (_isJumping && _characterController.isGrounded) _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravity);
 
-            transform.position += new Vector3(0, 1, 0) * _jumpSpeed * Time.deltaTime;
+            _playerVelocity.y += _gravity * Time.deltaTime;
+            _characterController.Move(_playerVelocity * Time.deltaTime);
         }
 
         // Event Handler Methods
@@ -71,5 +78,46 @@ namespace Player
         private void HandleSprint()          => _isSprinting = true;
         private void HandleSprintCancelled() => _isSprinting = false;
 
+    }
+}
+
+public class Example : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }

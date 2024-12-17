38fbd1e04949a94111cf95a84ba7511291c76e38
Elieser Capillar <elieser.capillar@hotmail.com>
2024-12-17 01:55:37 -0600

    Implemented Food Interaction Strategy

--- Assets/_Scripts/Player/Resources/PlayerResources.cs
+++ Assets/_Scripts/Player/Resources/PlayerResources.cs
@@ -7,16 +7,38 @@ public class PlayerResources : MonoBehaviour
 {
     [Header("Properties")]
     [SerializeField] private float _maxMilkCapacity = 100f;
+    [SerializeField] private float _maxFoodCapacity = 100f;
 
     // Resources
     private Observer<float> _currentMilk = new Observer<float>(0f);
+    private Observer<float> _currentFood = new Observer<float>(0f);
 
     public float MaxMilkCapacity           { get {return _maxMilkCapacity;} }
     public Observer<float> CurrentMilk     { get {return _currentMilk;} }
+    public float MaxFoodCapacity           { get {return _maxFoodCapacity;} }
+    public Observer<float> CurrentFood     { get {return _currentFood;} }
 
     private void Awake()
     {
         ServiceLocator.Global.Register(this);
+    }
+
+    public bool TryPourFood(float amount)
+    {
+        Debug.Log("Trying to pour milk...");
+        if (_currentFood <= 0) return false;
+
+        _currentFood.Value = Mathf.Clamp(_currentFood - amount, 0, _maxFoodCapacity);
+        return true;
+    }
+
+    public bool TryFillFood(float amount)
+    {
+        Debug.Log("Trying to fill milk...");
+        if (_currentFood > _maxFoodCapacity) return false;
+
+        _currentFood.Value = Mathf.Clamp(_currentFood + amount, 0, _maxFoodCapacity);
+        return true;
     }
 
     public bool TryPourMilk(float amount)

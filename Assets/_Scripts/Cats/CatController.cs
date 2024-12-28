23ff9ffa4be063fca38c46101ee17d1c18309df5
Elieser Capillar <elieser.capillar@hotmail.com>
2024-12-28 02:18:01 -0600

    Imported DoTween

---
+++ Assets/_Scripts/Cats/CatController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
